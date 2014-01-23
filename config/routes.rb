fcca5db8c04003ca2fb7d29bbdf9689ce0a30b5b
Nicolas <ferracano.nicolas@gmail.com>
2014-01-23 19:56:17 -0200

    Finish user sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 HiddenParadise::Application.routes.draw do
+	resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-get "users/new"
 
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
